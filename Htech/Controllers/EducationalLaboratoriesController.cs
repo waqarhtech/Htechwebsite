a9b919800796c2a5237af86b2a8c5afa5a3014c5
waqarhtech <waqar.htech@gmail.com>
2017-09-19 10:13:41 +0500

    Added All pages sucessfully

---
+++ Htech/Controllers/EducationalLaboratoriesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Htech.Controllers
+{
+    public class EducationalLaboratoriesController : Controller
+    {
+        // GET: EducationalLaboratories
+        public ActionResult EngineeringCollegeLabs()
+        {
+            return View();
+        }
+        public ActionResult NetworkingITLabs()
+        {
+            return View();
+        }
+        public ActionResult MedicalLabs()
+        {
+            return View();
+        }
+        public ActionResult SchoolCollegelabs()
+        {
+            return View();
+        }
+    }
+}
---
+++ Htech/Controllers/InstitutionalConsultancyController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Htech.Controllers
+{
+    public class InstitutionalConsultancyController : Controller
+    {
+        // GET: InstitutionalConsultancy
+        public ActionResult EtoE()
+        {
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        public ActionResult Affiliation()
+        {
+            return View();
+        }
+        public ActionResult Accreditation()
+        {
+            return View();
+        }
+    }
+ 
+}
